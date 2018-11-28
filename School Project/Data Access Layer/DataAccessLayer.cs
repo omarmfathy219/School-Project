3da9965aa12cf43cfda48915f0eb7713f244e271
Omar M. Fathy <OmarMFathy25@gmail.com>
2018-11-28 21:30:42 +0200

    Database Connection Added

---
+++ School Project/Data Access Layer/DataAccessLayer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace School_Project.Data_Access_Layer
+{
+    class DataAccessLayer
+    {
+        // ربط البرنامج (إنشاء الإتصال) بالداتا بيز
+        SqlConnection sqlconnection;
+
+        //Constructor to Initialize The Connection Object
+        public DataAccessLayer()
+        {                                   //Server Name; Database Name; Server Security //
+            sqlconnection = new SqlConnection(@"Server=.\; Database=School_DB; Integrated Security=true");
+        }
+
+        //Open Connection Method
+        public void Open()
+        {
+            if (sqlconnection.State != ConnectionState.Open)
+            {
+                sqlconnection.Open();
+            }
+        }
+        //Close Connection Method
+        public void Clsoe()
+        {
+            if (sqlconnection.State != ConnectionState.Closed)
+            {
+                sqlconnection.Close();
+            }
+        }
+    }
+}
